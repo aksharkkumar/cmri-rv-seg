a576dff75fba09b23ddb98ca540317f5445f1088
Akshar Kumar <ak.kumar93@gmail.com>
2018-01-15 23:08:24 -0800

    implements upsampling block of unet

--- src/unet.py
+++ src/unet.py
@@ -1,8 +1,8 @@
 import numpy as np
-from keras.layers import Input, Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Input, Conv2D, Conv2DTranspose, Cropping2D, MaxPooling2D, GlobalAveragePooling2D
 from keras.layers import Dropout, Flatten, Dense
 from keras.models import Model
-
+from keras import backend as K
 
 
 class UNet(object):
@@ -41,6 +41,36 @@ class UNet(object):
         layer = Conv2D(filters=features,kernel_size=3,activation='relu',padding='valid')(layer)
 
         #TODO implement upsampling portion
+        for i in reversed(range(steps)):
+            features //= 2
+            layer = Conv2DTranspose(filters=features,kernel_size=2,strides=2)(layer)
+            crop_copy = self.crop(layer,copies[i])
+            layer.Concatenate()( [layer, crop_copy] )
+
+            layer = Conv2D(filters=features,kernel_size=3,activation='relu',padding='valid')(layer)
+            layer = Conv2D(filters=features,kernel_size=3,activation='relu',padding='valid')(layer)
+
+        
+        outputs = Conv2D(filters=2,kernel_size=1,activation='softmax')(layer)
+
+        return Model(inputs=inputs,outputs=outputs)
 
 
+
+
+    def crop(self,layer,conv_copy):
+        _,layer_height,layer_width = K.int_shape(layer)
+        _,cc_height,cc_width = K.int_shape(conv_copy)
+
+        crop_height = cc_height - layer_height
+        crop_width = cc_width - layer_width
+
+
+        if crop_height == 0 and crop_width == 0:
+            copy = layer
+        else :
+            cropping = ((crop_height // 2, crop_height - crop_height//2), (crop_width, crop_width - crop_width//2))
+            copy = Cropping2D(cropping=cropping)(layer)
+
+        return copy
         
