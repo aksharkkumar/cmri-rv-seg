e8d027e55b96047d0ba20fdd5bd9161d211326f9
Akshar Kumar <ak.kumar93@gmail.com>
2018-03-06 21:40:36 -0800

    adds make one prediction

--- src/prediction.py
+++ src/prediction.py
@@ -7,36 +7,42 @@ import cv2
 from src import data, unet
 
 class Predictor(object):
-
-    def make_predictions(self,data_dir,out_dir):
+    def __init__(self,data_dir):
         glob_search = os.path.join(data_dir,"patient*")
-        patient_dirs = sorted(glob.glob(glob_search))
-
-        images, _, _ = self.load_images(patient_dirs[0])
+        self.patient_dirs=sorted(glob.glob(glob_search))
+        images, _, _ = self.load_images(self.patient_dirs[0])
         _, height, width, channels = images.shape
-        print("Building models...")
-        o_model = unet.UNet().get_unet(height=height,width=width,channels=channels,features=32,steps=3)
-        i_model = unet.UNet().get_unet(height=height,width=width,channels=channels,features=32,steps=3)
+        self.o_model = unet.UNet().get_unet(height=height,width=width,channels=channels,features=32,steps=3)
+        self.i_model = unet.UNet().get_unet(height=height,width=width,channels=channels,features=32,steps=3)
+        self.o_model.load_weights('notebooks/saved_models/endo_models/weightsNoDrop.hdf5')
+        self.i_model.load_weights('notebooks/saved_models/epi_models/weightsNoDrop.hdf5')
 
-        o_model.load_weights('notebooks/saved_models/endo_models/weightsNoDrop.hdf5')
-        i_model.load_weights('notebooks/saved_models/epi_models/weightsNoDrop.hdf5')
-
-        for path in patient_dirs:
+    def make_predictions(self,out_dir):
+        for path in self.patient_dirs:
             images,p_ids,rotated = self.load_images(path)
             o_predictions=[]
             i_predictions=[]
             for image in images:
-                o_mask_pred = o_model.predict(image[None,:,:,:])
-                i_mask_pred = i_model.predict(image[None,:,:,:])
+                o_mask_pred = self.o_model.predict(image[None,:,:,:])
+                i_mask_pred = self.i_model.predict(image[None,:,:,:])
                 o_predictions.append((image[:,:,0],o_mask_pred[0,:,:,1]))
                 i_predictions.append((image[:,:,0],i_mask_pred[0,:,:,1]))
             self.save_predictions(o_predictions,p_ids,rotated,"o",out_dir)
             self.save_predictions(i_predictions,p_ids,rotated,"i",out_dir)
-            
+
         
         return o_predictions, i_predictions
 
-
+    def make_predictions_one(self,data_dir):
+        images, _, _ = self.load_images(data_dir)
+        o_predictions = []
+        i_predictions = []
+        for image in images:
+            o_mask_pred = self.o_model.predict(image[None,:,:,:])
+            i_mask_pred = self.i_model.predict(image[None,:,:,:])
+            o_predictions.append((image[:,:,0],o_mask_pred[0,:,:,1]))
+            i_predictions.append((image[:,:,0],i_mask_pred[0,:,:,1]))
+        return o_predictions,i_predictions
 
     def load_images(self,path):
         img_data_obj = data.ImageData(path)
